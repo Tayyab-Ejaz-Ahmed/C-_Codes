5b425d9c0c01eaba6bb5ea0058c2268f77febcee
Tayyab-Ejaz-Ahmed <165308196+Tayyab-Ejaz-Ahmed@users.noreply.github.com>
2024-04-27 13:38:49 +0500

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
+void function() {
+cout << "This is the another changes in the file" << endl;
+}
 int main() {
   cout << "This will work as the pull request" << endl;
+  function();
 return 0;
 }
